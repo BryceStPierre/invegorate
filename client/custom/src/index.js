48dc103926c95336ea7167635abac069923c940f
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-06-17 19:50:21 -0400

    Added proxy to webpack-dev-server and added babel support.

--- client/custom/src/index.js
+++ client/custom/src/index.js
@@ -1,1 +1,5 @@
-alert('Hello world!!!');
+fetch('/api/users')
+  .then(res => res.json())
+  .then(users => {
+    alert(`Hello user ${users[0].id}: ${users[0].data}`);
+  });
--- client/custom/webpack.config.js
+++ client/custom/webpack.config.js
@@ -1,16 +1,30 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const ENV = process.env.NODE_ENV;
 
 module.exports = {
   entry: {
     'custom': './src/index.js'
   },
-  devtool: 'inline-source-map',
+  devtool: ENV === 'production' ? false : 'inline-source-map',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    proxy: {
+      '/api': 'http://localhost:5000'
+    }
   },
-  mode: process.env.NODE_ENV,
+  mode: ENV,
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new UglifyJsPlugin()
+  ]
 };
