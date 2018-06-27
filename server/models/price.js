77083d0a0ea7d215b6bd93b8b1e6fc306354edfd
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-06-27 16:42:19 -0400

    Began model for price API.

--- server/models/price.js
+++ server/models/price.js
@@ -1,5 +1,21 @@
+var request = require('request-promise');
+
 class Price {
 
+  static retrieveByName (name, postalCode, callback) {
+
+    const testName = `broccoli`;
+    const testPostalCode = `N8N4T3`;
+
+    request({
+      uri: `https://gateflipp.flippback.com/bf/flipp/items/search?locale=en-ca&postal_code=${testPostalCode}&q=${testName}`,
+      json: true
+    }).then(function (res) {
+      callback(null, res);
+    }).catch(function (err) {
+      console.log(err);
+    });
+  } 
 }
 
 module.exports = Price;
