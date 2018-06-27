5427f70393442d3dc61b24a33af6661355b58a6d
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-06-26 21:08:37 -0400

    Established beginning of climate API.

--- server/api/climate.js
+++ server/api/climate.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var Climate = require('../models/climate');
+
 router.get('/', function (req, res) {
-  
+  Climate.retrieveByName('', function (err, climate) {
+    res.json(climate);
+  })
 });
 
 module.exports = router;
--- server/models/climate.js
+++ server/models/climate.js
@@ -1,7 +1,32 @@
+var request = require('request-promise');
+var parse = require('csv-parse');
 
 class Climate {
   static retrieveByName (name, callback) {
-    
+
+    request({
+      uri: `http://climate.weather.gc.ca/climate_normals/bulk_data_e.html?ffmt=csv&lang=e&prov=ON&yr=1981&stnID=4607&climateID=6130257+++++++++++++&submit=Download+Data`
+    }).then(function (res) {
+
+      // Trim header lines and problematic lines.
+      var lines = res.split('\n');
+      lines.splice(0, 13);
+      lines.splice(1, 1);
+      lines.splice(9, 1);
+
+      parse(lines.join('\n'), {
+        from: 0,
+        to: 19,
+        columns: true
+      }, function (err, output) {
+        if (err)
+          console.log(err);
+        callback(null, output);
+      });
+
+    }).catch(function (err) {
+      console.log(err);
+    });
   }
 }
 
