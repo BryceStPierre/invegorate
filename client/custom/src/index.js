83cd19145aebe18654948cec65a91fa479b9bc34
Bryce St. Pierre <brycelstpierre@gmail.com>
2018-08-01 21:15:23 -0400

    Updated non-React client interface.

--- client/custom/src/index.js
+++ client/custom/src/index.js
@@ -1,8 +1,9 @@
 import './semantic/dist/semantic.min.css';
+import './style.css';
 import './semantic/dist/semantic.min.js';
 
-fetch('/api/users')
-  .then(res => res.json())
-  .then(users => {
-    alert(`Hello user ${users[0].id}: ${users[0].data}`);
-  });
+// fetch('/api/users')
+//   .then(res => res.json())
+//   .then(users => {
+//     alert(`Hello user ${users[0].id}: ${users[0].data}`);
+//   });
